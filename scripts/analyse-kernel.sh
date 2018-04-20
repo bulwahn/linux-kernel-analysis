9ce0f63a0ddb0d7196c0634efc2b5b2fb6d6c9a0
Ozan Alpay <ozanalpay@yandex.com.tr>
2018-04-20 16:03:15 +0200

    Required modifications made to meet requirements, by default run analyze after capture, run without capture option added

--- scripts/analyse-kernel.sh
+++ scripts/analyse-kernel.sh
@@ -1,36 +1,28 @@
 #!/bin/bash
 
 help() {
-	echo "Usage: $0 [-r] [-s] [-c] [-i]"
+	echo "Usage: $0 [-r] [-c] [-i]"
 	echo "Or you can directly provide a analysisconfig file with [--configfile]"
 	echo "  -r,  kernel-repository"
-	echo "  -s,  kernel-sha"
 	echo "  -c,  kernel-configuration"
-	echo "  -i,  inferconfig-file location"
+	echo "  optional -i,  inferconfig-file location"
 	echo "  --configfile, analysisconfig file for build"
-	echo "Example: $0 -r linux-stable -s d8a5b80568a9 -c defconfig -i [absolute-path-to-file]"
-	echo "Example with analysisconfig file: $0 --configfile [absolute-path-to-file]"
+	echo "  --no-analyze, don't run infer analyze after infer capture finishes"
+	echo "Example: $0 -r stable -c defconfig -i [absolute-path-to-file]"
+	echo "Example with analysisconfig file: $0 --configfile files/analysisconfig"
+	echo "$0 --configfile [full-path-to-file] example: ./analysisconfig --configfile /home/x/y/z/analysisconfig"
 	echo "Before use it, please be sure you set KERNEL_SRC_BASE variable correctly"
 	exit 1
-}
-assign_default_values() {
-	KERNEL_REPOSITORY="$KERNEL_SRC_BASE/stable/linux-stable"
-	KERNEL_VERSION="d8a5b80568a9"
-	KERNEL_CONFIG="defconfig"
-	INFERCONFIG_LOCATION="$SCRIPTS_DIRECTORY/files/inferconfig"
 }
 set_kernel_repository() {
 	if [ ! -z "$1" ]; then
 		case $1 in
 			torvalds | stable | next )
 			if [ "$1" == "torvalds" ]; then
-				echo "Torvalds!"
 				KERNEL_REPOSITORY="$KERNEL_SRC_BASE/torvalds/linux"
 			elif [ "$1" == "stable" ]; then
-				echo "Stable!"
 				KERNEL_REPOSITORY="$KERNEL_SRC_BASE/stable/linux-stable"
 			elif [ "$1" == "next" ]; then
-				echo "Next!"
 				KERNEL_REPOSITORY="$KERNEL_SRC_BASE/next/linux-next"
 			else
 				echo "Invalid Kernel Repository Parameter!"
@@ -40,8 +32,7 @@ set_kernel_repository() {
 		esac
 	else
 		echo "You provided an empty KERNEL_REPOSITORY parameter!"
-		echo "Script will use linux-stable repository for this run!"
-		KERNEL_REPOSITORY="$KERNEL_SRC_BASE/stable/linux-stable"
+		echo "Acceptable parameters are =  torvalds | stable | next"
 		exit 1;
 	fi
 }
@@ -52,21 +43,28 @@ set_kernel_config() {
 			KERNEL_CONFIG="$1"
 			;;
 	*)
-		echo "You provided a KERNEL_CONFIG parameter but it is not a valid kernel configuration!"
-		exit 1
+		echo "You provided a KERNEL_CONFIG parameter but it is not valid!"
+		echo "Valid Parameters are = allnoconfig | allmodconfig | allyesconfig | defconfig | randconfig"
+		exit 1;
 	esac
 	else
-		echo "You provided an empty KERNEL_CONFIG parameter, script will use default value" ##Can it happen in practice??
-		KERNEL_CONFIG="defconfig"
+		echo "You provided an empty KERNEL_CONFIG parameter" ##Can it happen in practice??
+		echo "Valid Parameters are = allnoconfig | allmodconfig | allyesconfig | defconfig | randconfig"
+		exit 1;
 	fi
 }
 set_inferconfig_file_location() {
-	if  [ -f "$1" ]; then
+	if [ -f "$SCRIPTS_DIRECTORY/$1" ]; then
+		INFERCONFIG_LOCATION="$SCRIPTS_DIRECTORY/$1"
+	elif [ -f "$1" ]; then
 		INFERCONFIG_LOCATION="$1"
-	else
-		echo "Couldn't read inferconfig file, will use default"
-		INFERCONFIG_LOCATION= "$SCRIPTS_DIRECTORY/files/inferconfig"
 	fi
+}
+set_scripts_directory() {
+	SCRIPTS_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+}
+remove_analyze_command() {
+	RUN_COMMAND=${RUN_COMMAND/" && infer analyze"}
 }
 read_and_set_variables_from_analysisconfig() {
 	if [ -f "$1" ]; then
@@ -76,45 +74,86 @@ read_and_set_variables_from_analysisconfig() {
 		exit 1;
 	fi
 }
+check_kernel_src_base_valid() {
+	if [ -z "$KERNEL_SRC_BASE" ]; then 
+		echo "Please set KERNEL_SRC_BASE first"
+		help
+		exit 1
+	elif [ ! -d "$KERNEL_SRC_BASE" ]; then
+		echo "KERNEL_SRC_BASE does not point to a directory"
+		help
+		exit 1
+	fi
+}
+check_kernel_repository_valid() {
+	if [ -z "$KERNEL_REPOSITORY" ]; then
+		echo "You must provide a target kernel repository"
+		echo "Valid parameters are =  torvalds | stable | next"
+		exit 1;
+	elif [ ! -d "$KERNEL_REPOSITORY" ]; then # Check KERNEL_REPOSITORY is a directory or not
+		echo "Path $KERNEL_REPOSITORY doesn't point to a directory"
+		echo "Please fix it and then run script"
+		exit 1
+	fi
+}
+check_kernel_configuration_valid() {
+	if [ -z "$KERNEL_CONFIG" ]; then # Check KERNEL_CONFIG variable is set
+		echo "You must provide a valid kernel configuration"
+		echo "Valid Parameters are = allnoconfig | allmodconfig | allyesconfig | defconfig | randconfig"
+		exit 1;
+	fi
+}
+check_inferconfig_exists() {
+	#Infer always start looking from root of target file directory to find an .inferconfig file, I am not sure we can give it as a parameter???
+	if [ -z "$INFERCONFIG_LOCATION" ] && [ ! -f "$KERNEL_REPOSITORY/.inferconfig" ]; then 
+		echo "You should have a .inferconfig file in the root of linux-source repository"
+		echo "Or you must provide a valid .inferconfig file path with -i parameter"
+		exit 1;
+	elif [ ! -z "$INFERCONFIG_LOCATION" ]; then
+		cp "$INFERCONFIG_LOCATION" "$KERNEL_REPOSITORY/.inferconfig"
+	else
+		echo "Script will use .inferconfig file , that exists in $KERNEL_REPOSITORY!"
+	fi
+}
+can_apply_patch() {
+	APPLY_RESULT=$(git apply "$SCRIPTS_DIRECTORY/files/0001-Set-default-CC-to-Clang-from-Makefile.patch" 2>&1 )
+	if [ -n "$APPLY_RESULT" ]; then
+		echo "$APPLY_RESULT"
+		echo "Failed to Apply 0001-Set-default-CC-to-Clang patch."
+		echo "Please check your linux source directory"
+		exit 1;
+	fi
+}
+does_user_need_help() {
+	if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+		help
+		exit 0
+	fi
+}
+## MAIN ##
 # Check KERNEL_SRC_BASE
-if [ -z "$KERNEL_SRC_BASE" ]; then 
-	echo "Please set KERNEL_SRC_BASE first"
-	help
-	exit 1
-elif [ ! -d "$KERNEL_SRC_BASE" ]; then
-	echo "KERNEL_SRC_BASE does not point to a directory"
-	help
-	exit 1
-fi
-SCRIPTS_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-#KERNEL_STABLE_DIRECTORY="$KERNEL_SRC_BASE/stable/linux-stable"
-# User need help or called without params
-if [[ "$1" == "-h" || "$1" == "--help" ]]; then
-	help
-	exit 0
-fi
-assign_default_values
+check_kernel_src_base_valid
+set_scripts_directory
+does_user_need_help "$1"
+RUN_COMMAND="cd linux && make clean && make $KERNEL_CONFIG && infer capture -- make -j40 && infer analyze"
+# Get Parameters, validate them, assign them variables.
 while [[ "$#" > 0 ]]; do case $1 in
   -r) set_kernel_repository "$2"; shift; shift;;
-  -s) KERNEL_HEAD_SHA="$2"; shift; shift;;
   -c) set_kernel_config "$2"; shift; shift;;
   -i) set_inferconfig_file_location "$2"; shift; shift;;
   --configfile) read_and_set_variables_from_analysisconfig "$2"; shift; shift;;
+  --no-analyze) remove_analyze_command; shift; shift;;
   *) help; shift; shift; exit 1;;
 esac; done
-if [ ! -d "$KERNEL_REPOSITORY" ]; then
-	echo "Path $KERNEL_REPOSITORY doesn't point to a directory"
-	echo "Please fix it and then run script"
-	exit 1
-fi
-echo "KERNEL_REPOSITORY is = $KERNEL_REPOSITORY"
+# Check KERNEL_REPOSITORY variable is set
+check_kernel_repository_valid
+check_kernel_configuration_valid
+check_inferconfig_exists
 cd $KERNEL_REPOSITORY
-#git clean -f -d
-#git checkout .
-git checkout $KERNEL_HEAD_SHA
-cp "$INFERCONFIG_LOCATION" "$KERNEL_REPOSITORY/.inferconfig"
-git apply "$SCRIPTS_DIRECTORY/files/0001-Set-default-CC-to-Clang-from-Makefile.patch"
+if [ ! -z "$KERNEL_HEAD_SHA" ]; then
+	git checkout $KERNEL_HEAD_SHA
+fi
+can_apply_patch
 DOCKER_NAME="kernel-analysis"
 docker run -v "$KERNEL_REPOSITORY:/linux/" --interactive --tty $DOCKER_NAME \
-/bin/sh -c "cd linux && make clean && make $KERNEL_CONFIG && infer capture -- make -j40 && git checkout . && git checkout master && rm .inferconfig"
-
+/bin/sh -c "$RUN_COMMAND"
