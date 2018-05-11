2d4f9d04b058166cda3c6a4c45935cdfc8cf6bd9
Ozan Alpay <ozanalpay@yandex.com.tr>
2018-05-11 16:53:03 +0200

    Dockerfile updated for Infer Version 0.14.0, script modified

--- scripts/analyse-kernel.sh
+++ scripts/analyse-kernel.sh
@@ -47,10 +47,6 @@ set_kernel_config() {
 		echo "Valid Parameters are = allnoconfig | allmodconfig | allyesconfig | defconfig | randconfig"
 		exit 1;
 	esac
-	else
-		echo "You provided an empty KERNEL_CONFIG parameter" ##Can it happen in practice??
-		echo "Valid Parameters are = allnoconfig | allmodconfig | allyesconfig | defconfig | randconfig"
-		exit 1;
 	fi
 }
 set_inferconfig_file_location() {
@@ -70,6 +66,12 @@ finalize_command() {
 	if [ "$DONT_RUN_ANALYZE" == "1" ]; then
 		RUN_COMMAND=${RUN_COMMAND/" && infer analyze"}
 	fi
+	if [ -z "$KERNEL_CONFIG" ]; then ## Means user didn't provided any kernel-configuration parameter
+		echo "You didnt provided a Kernel Configuration Parameter!"
+		echo "Script will use .config file, that inside $KERNEL_REPOSITORY"
+		RUN_COMMAND=${RUN_COMMAND/"make  &&"}
+	fi
+	echo "RUN COMMAND is = $RUN_COMMAND"
 }
 read_and_set_variables_from_analysisconfig() {
 	if [ -f "$1" ]; then
@@ -99,20 +101,20 @@ check_kernel_repository_valid() {
 		set_kernel_repository "$KERNEL_REPOSITORY"
 	fi
 }
-check_kernel_configuration_valid() {
-	if [ -z "$KERNEL_CONFIG" ]; then # Check KERNEL_CONFIG variable is set
-		echo "You must provide a valid kernel configuration"
-		echo "Valid Parameters are = allnoconfig | allmodconfig | allyesconfig | defconfig | randconfig"
-		exit 1;
-	fi
-}
+#check_kernel_configuration_valid() {
+#	if [ -z "$KERNEL_CONFIG" ]; then # Check KERNEL_CONFIG variable is set
+#		echo "You must provide a valid kernel configuration"
+#		echo "Valid Parameters are = allnoconfig | allmodconfig | allyesconfig | defconfig | randconfig"
+#		exit 1;
+#	fi
+#}
 check_inferconfig_exists() {
 	if [ -f "$INFERCONFIG_LOCATION" ]; then # Highest priority is parameter
 		cp "$INFERCONFIG_LOCATION" "$KERNEL_REPOSITORY/.inferconfig"
 	elif [ -f "$SCRIPTS_DIRECTORY/$INFERCONFIG_LOCATION" ]; then #Second priority is analysisconfig file
 		cp "$SCRIPTS_DIRECTORY/$INFERCONFIG_LOCATION" "$KERNEL_REPOSITORY/.inferconfig"
-	elif [ -f "$KERNEL_REPOSITORY/.inferconfig"]; then #If still we couldn't a valid inferconfig file, but there is a .inferconfig in source-repository use it instead of raising an error
-		echo "Script will use .inferconfig file, tha already exists in $KERNEL_REPOSITORY"
+	elif [ -f "$KERNEL_REPOSITORY/.inferconfig" ]; then #If still we couldn't a valid inferconfig file, but there is a .inferconfig in source-repository use it instead of raising an error
+		echo "Script will use .inferconfig file, that already exists in $KERNEL_REPOSITORY"
 	else
 		echo "You should provide a .inferconfig file in the root of linux-source repository"
 		echo "Or you must provide a valid inferconfig file path"
@@ -163,7 +165,7 @@ esac; done
 RUN_COMMAND="cd linux && make clean && make $KERNEL_CONFIG && infer capture -- make -j40 && infer analyze"
 # Check KERNEL_REPOSITORY variable is set
 check_kernel_repository_valid
-check_kernel_configuration_valid
+# check_kernel_configuration_valid
 check_inferconfig_exists
 finalize_command
 cd $KERNEL_REPOSITORY
